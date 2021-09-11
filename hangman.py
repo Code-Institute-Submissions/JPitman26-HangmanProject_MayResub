208ba39918d512bd360ae39f5635b61ae3aeaa51
JackPitman <80924402+JPitman26@users.noreply.github.com>
2021-09-11 11:15:22 +0100

    update hangman.py
    Corrected formatting errors found when using the pep8 checker

--- hangman.py
+++ hangman.py
@@ -8,31 +8,32 @@ def get_word(word_list):
 # Uppercase for terminal readability
     return word.upper()
 
+
 # Displays word during the turn
 def play(word):
-# Hidden guesses are displayed with _
+    # Hidden guesses are displayed with _
     word_completion = "_" * len(word)
     guessed = False
     guessed_letters = []
     guessed_words = []
-# Amount of tries the user has, based on the original hangman body parts
+    # Amount of tries the user has, based on the original hangman body parts
     tries = 8
     print("Guess The Word To Win Hangman!")
     print(word_completion)
     print("\n")
     while not guessed and tries > 0:
-# Prompts the user to start guessing
+        # Prompts the user to start guessing
         guess = input("Guess a letter or word: ").upper()
         if len(guess) == 1 and guess.isalpha():
-# Displays if a letter is already guessed
+            # Displays if a letter is already guessed
             if guess in guessed_letters:
                 print("You already guessed", guess,)
-# Displays if the letter guessed is not in the word a try is deducted as penalty
+            # Displays if the letter guessed is not in the word a try is deducted as penalty
             elif guess not in word:
                 print(guess, "isn't in the word")
                 tries -= 1
                 guessed_letters.append(guess)
-# Displays when the user has guessed a correcct letter, this letter will replace the _
+            # Displays when the user has guessed a correcct letter, this letter will replace the _
             else:
                 print("Got one!,", guess, "is in the word!")
                 guessed_letters.append(guess)
@@ -44,10 +45,10 @@ def play(word):
                 if "_" not in word_completion:
                     guessed = True
         elif len(guess) == len(word) and guess.isalpha():
-# Displays when the user guesses a full word that they had previously guesed
+            # Displays when the user guesses a full word that they had previously guesed
             if guess in guessed_words:
                 print("You already tried ", guess)
-# Displays when the user guesses an incorrect full word a try is deducted as penalty
+            # Displays when the user guesses an incorrect full word a try is deducted as penalty
             elif guess != word:
                 print(guess, " is not the correct word")
                 tries -= 1
@@ -59,10 +60,10 @@ def play(word):
             print("invalid input")
         print(word_completion)
         print("\n")
-# Displays when the user has indevidualy guessed the letters or the full word
+    # Displays when the user has indevidualy guessed the letters or the full word
     if guessed:
         print("Well Done, you guessed the word!")
-# Displays when the user has tried more than 8 times
+    # Displays when the user has tried more than 8 times
     else:
         print("You ran out of tries. The word was " + word + ". Better luck next time")
 
